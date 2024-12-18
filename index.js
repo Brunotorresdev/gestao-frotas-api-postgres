5559572618aed7247310145d909fda51df9c4a32
Brunotorresdev <brunotorresreal@gmail.com>
2024-12-18 10:14:09 -0300

    config vercel

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const routes = require('./src/routes');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');;
-const swaggerFile = require('./swagger-output.json');
+const express = require("express");
+const dotenv = require("dotenv");
+const routes = require("./src/routes");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 
 dotenv.config();
 
 const app = express();
-
 
 const port = process.env.PORT;
 
@@ -16,9 +15,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
-app.get('/', (req, res) => res.json({ message: 'Hello world!' }));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.get("/", (req, res) => res.json({ message: "Hello world!" }));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.listen(port,() => {
+module.exports = app;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
     console.log(`Server is running in http://localhost:${port}`);
-});
+  });
+}
